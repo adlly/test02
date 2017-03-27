08d098b7c2f1c3c9f3c8a453ef337d833520e5e2
ad <ad@itmp.top>
2017-03-27 17:08:01 +0800

    in  & not in

--- test01.py
+++ test01.py
@@ -1,42 +1,20 @@
-#/usr/bin/python
+#!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+a = 10
+b = 20
+list = [1,2,3,4,5];
+if(a in list):
+    print "a in list"
+else:
+    print "a not in list"
+if(b not in list):
+    print "b not in list"
+else:
+    print "b in the list"
 
-# 列表用[]表示
-# '+'表示连接,'*'表示重复
-
-list = ['aaa', 456,65.25,'dks',25.01]
-tinylist = ['dfd',4556]
-
-print list
-print list[0]
-print list[1:3]
-print list[2:]
-print tinylist * 2
-print list+tinylist
-
-
-#元组用()表示,元组不可二次赋值,相当于只读列表
-
-tuple = ('runoob',456,2.23,'john',213.454)
-tinytuple = ('1sdfs',15431)
-
-print tuple
-print tuple[0]
-print tuple[1:3]
-print tuple[2:]
-print tinytuple*2
-print tuple+tinytuple
-
-#字典 {}表示  列表时有序的对象集合,字典是无序的对象集合
-
-dict = {}
-dict['one'] = "this is a shit"
-dict[2] = "this is an another shit"
-tinydict = {'name':'join','code':4572,'dept':'sdfs'}
-
-print dict['one']
-print dict[2]
-print tinydict
-print tinydict.keys()
-print tinydict.values()
+a = 2
+if(a in list):
+    print "a in the list"
+else:
+    print "a not in the list"
