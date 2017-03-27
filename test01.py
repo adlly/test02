21bedf5a19b6e241e9e9fa7f514838caca8ab1af
ad <ad@itmp.top>
2017-03-27 16:00:50 +0800

    基本类型

--- test01.py
+++ test01.py
@@ -1,35 +1,42 @@
-#!/usr/bin/python
-# -*- coding=utf-8 -*-
-
-str = "Hello World!"
-
-print str
-print str[0]
-print str[2:5]
-print str[2:]
-print str * 2
-print str + "TEST"
+#/usr/bin/python
+# -*- coding: UTF-8 -*-
 
 
+# 列表用[]表示
+# '+'表示连接,'*'表示重复
+
+list = ['aaa', 456,65.25,'dks',25.01]
+tinylist = ['dfd',4556]
+
+print list
+print list[0]
+print list[1:3]
+print list[2:]
+print tinylist * 2
+print list+tinylist
 
 
+#元组用()表示,元组不可二次赋值,相当于只读列表
 
-x = 'a'
-y = 'b'
+tuple = ('runoob',456,2.23,'john',213.454)
+tinytuple = ('1sdfs',15431)
 
-print x
-print y
+print tuple
+print tuple[0]
+print tuple[1:3]
+print tuple[2:]
+print tinytuple*2
+print tuple+tinytuple
 
-print 'blabla'
+#字典 {}表示  列表时有序的对象集合,字典是无序的对象集合
 
-print x,
-print y
+dict = {}
+dict['one'] = "this is a shit"
+dict[2] = "this is an another shit"
+tinydict = {'name':'join','code':4572,'dept':'sdfs'}
 
-a, b, c = 1, 2, "addy"
-
-print a,
-print b,
-print c
-
-del a,b
-
+print dict['one']
+print dict[2]
+print tinydict
+print tinydict.keys()
+print tinydict.values()
