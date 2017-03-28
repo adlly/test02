bffac2c000cfe31388c64a07a4a43dde20bd350d
adlly <mm@ifnot.cc>
2017-03-28 21:18:01 +0800

    for循环

--- test01.py
+++ test01.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python
 # -*- coding:UTF-8 -*-
 
-numbers = [12, 37, 5, 42, 8, 3]
-even = []
-odd = []
+for letter in 'Python':
+    print '当前字母：'+ letter
+#    print('当前字母：',letter)
 
-while len(numbers) > 0:
-    number = numbers.pop()
-    if(number % 2 == 0):
-        even.append(number)
-    else:
-        odd.append(number)
-
-
-print(even)
-
-print(odd)
+fruits = ['banana', 'apple', 'mango']
+for fruit in fruits:
+    print '当前水果：'+fruit
+print "good bye!"
