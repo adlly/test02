6069f59dc46844de26cc636142deb380478edf46
ad <ad@itmp.top>
2017-03-30 15:56:38 +0800

    continue

--- test01.py
+++ test01.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
 # -*- coding:UTF-8 -*-
 
-for letter in 'Python':
-    if letter == 'h':
-        break
-    print 'Current Letter :',letter
+for letters in 'Python':
+    if letters == 'h':
+        continue
+    print '当前字母: ' + letters
 
 var = 10
-
 while var > 0:
-    print 'Current variable value :',var
     var = var - 1
-    if var == 5:
-        break
-
+    if(var == 5):
+        continue
+    print '当前变量值: ' , var
 print "Good bye!"
