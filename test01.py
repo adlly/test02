9e1519a92a58feaf0398c342ae902ccf19882a45
ad <ad@itmp.top>
2017-03-31 10:07:03 +0800

    string1

--- test01.py
+++ test01.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python
 # -*- coding:UTF-8 -*-
 
-for letters in 'Python':
-    if letters == 'h':
-        continue
-    print '当前字母: ' + letters
+aa = "abc"
+bb = "efg"
 
-var = 10
-while var > 0:
-    var = var - 1
-    if(var == 5):
-        continue
-    print '当前变量值: ' , var
-print "Good bye!"
+print "aa[0]: ", aa[0]
+
+print "bb[0:]", bb[0:]
+
+
+cc = 'Hello World!\a'
+print "更新字符串: -",cc[:6]+""" ksjdfldjs"""
+
+
+dd = 'a'\
+    'b'\
+    'c'\
+    'd'
+print cc
+
+print dd
