8d7d03b5dfcb0c0efe320ebac0594cfc010680a0
ad <ad@itmp.top>
2017-04-01 14:14:51 +0800

    func3 缺省参数

--- test01.py
+++ test01.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 # -*- coding:UTF-8 -*-
 
-def printinfo(name, age):
-    "打印任何传入的字符串"
+def printinfo(name, age=15):
     print "Name: ",name
     print "Age: ",age
     return
 
-printinfo(age = 50, name = "miki");
-
+printinfo(age = 50, name = "miki")
+printinfo(name="miki")
