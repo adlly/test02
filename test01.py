7a488f6f94596d3898b92d80a95bd662b65aa570
ad <ad@itmp.top>
2017-04-01 15:13:29 +0800

    局部 全局变量1

--- test01.py
+++ test01.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding:UTF-8 -*-
 
-sum = lambda arg1, arg2: arg1 + arg2
-
-print "相加后的值为: ", sum(10,20)
-print "相加后的值为: ", sum(20,30)
+Money = 2000
+def AddMoney():
+    global Money
+    Money = Money + 1
+print Money
+AddMoney()
+print Money
